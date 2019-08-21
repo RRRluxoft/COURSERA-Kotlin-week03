bb7144772dfd3373172faaac356872832ef464e3
Roman <rrr.lands@gmail.com>
2019-08-21 18:05:04 +0200

    Kotlin. week03 - TaxiPark - smth success

--- Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -1,7 +1,5 @@
 package taxipark
 
-import kotlin.math.min
-
 /*
  * Task #1. Find all the drivers who performed no trips.
  */
@@ -12,29 +10,46 @@ fun TaxiPark.findFakeDrivers(): Set<Driver> {
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> {
-    val tripsSuccess = this.trips.groupBy { t -> t.driver }.filterValues { it.size >= minTrips }
+//    val tripsSuccess = this.trips.groupBy { t -> t.driver }.filterValues { it.size >= minTrips }
 //    if (tripsSuccess.size >= minTrips) return tripsSuccess.flatMap { it. }.toSet()
-    return emptySet()
+//    val trips: List<Trip> = tripsSuccess.values.flatMap { it }
+    val happyPasses: List<Passenger> = this.trips.flatMap { it.passengers }
+    val mapPassengerPerTrip: Map<Passenger, Int> = happyPasses
+            .groupingBy { it  }
+            .eachCount()
+            .filter { it.value >= minTrips }
+    return mapPassengerPerTrip.keys
 }
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
-fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-        TODO()
-
+fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> {
+    val tripsSuccess = this.trips.groupBy { t -> t.driver }
+//return tripsSuccess[driver].flatMap { it.passengers }.toSet()
+    val tripByDriver: List<Trip>? = tripsSuccess[driver]
+    return tripByDriver!!.map { it.passengers }
+            .flatten()
+            .groupingBy { it }
+            .eachCount()
+            .filter { it.value > 1 }.keys
+}
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
-fun TaxiPark.findSmartPassengers(): Set<Passenger> =
-        TODO()
+fun TaxiPark.findSmartPassengers(): Set<Passenger> {
+        val allPasses = this.trips.filter { it.discount != null }.flatMap { it.passengers }
+        return allPasses.toSet()
+}
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    return TODO()
+    val durationList: List<Int> = this.trips.map { it.duration }
+    val windows: List<List<Int>> = durationList.windowed(10, 1 )
+    return  (0 until 10)
 }
 
 /*
